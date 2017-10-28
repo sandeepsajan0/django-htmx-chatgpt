63ccfc424b4fb83953c9d3e5d2e3598e486f587b
sandeepsajan0 <sandeepsajan0@gmail.com>
2017-10-29 01:11:26 +0530

    update profile access

--- login_account/views.py
+++ login_account/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.forms import UserCreationForm
 from login_account.forms import RegistrationForm, EditProfileForm
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.decorators import login_required
 
 def hello(request):
     number=[1,2,3,4,5]
@@ -23,12 +24,12 @@ def register(request):
 		form = RegistrationForm()
 		args = {'form': form}
 		return render (request,'login_account/reg_form.html',args)
-
+@login_required
 def profile(request):
 	args = {'user': request.user}
 	return render (request,'login_account/profile.html',args)
 
-
+@login_required
 def edit_profile(request):
 	if request.method == "POST":
 		form = EditProfileForm(request.POST, instance = request.user)
