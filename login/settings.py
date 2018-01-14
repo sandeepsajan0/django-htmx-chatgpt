cd08a3945a82769871a33806812df06a20145144
sandeepsajan0 <sandeepsajan0@gmail.com>
2018-01-15 01:52:51 +0530

    fix error

--- login/settings.py
+++ login/settings.py
@@ -74,6 +74,8 @@ WSGI_APPLICATION = 'login.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 DATABASES = {
