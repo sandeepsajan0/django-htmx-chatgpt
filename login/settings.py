7829d7082b87d2421dee420f89fd36164717bd8f
sandeepsajan0 <sandeepsajan0@gmail.com>
2018-01-16 03:25:49 +0530

    Ready to go to Heroku

--- login/settings.py
+++ login/settings.py
@@ -20,6 +20,7 @@ STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '16=e$ymxg^7o4ex4k_*#96^4*pku64c(g311q8n8fw2k+lo5y*'
 
