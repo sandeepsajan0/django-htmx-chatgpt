c9aa7f475e41732a063dd10f74ae4ae9c822060b
sandeepsajan0 <sandeepsajan0@gmail.com>
2018-01-15 22:27:37 +0530

    update host

--- login/wsgi.py
+++ login/wsgi.py
@@ -7,10 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
+
+
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "<PROJECT_NAME>.settings")
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "login.settings")
 
 application = get_wsgi_application()
