b7902c11062ea16e0ade0d1e887373c122641bfb
sandeepsajan0 <sandeepsajan0@gmail.com>
2018-01-20 03:15:49 +0530

    change db

--- login/wsgi.py
+++ login/wsgi.py
@@ -13,6 +13,5 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "login.settings")
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
 
-application = Cling(get_wsgi_application())
+application = (get_wsgi_application())
