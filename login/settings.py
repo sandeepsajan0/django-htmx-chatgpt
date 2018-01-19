a64dd0217a0341bdd9cc9728c67874b018e5a081
sandeepsajan0 <sandeepsajan0@gmail.com>
2018-01-20 03:51:59 +0530

    change db

--- login/settings.py
+++ login/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -75,19 +76,23 @@ WSGI_APPLICATION = 'login.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-import dj_database_url
+#import dj_database_url
 
-DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
-#        'NAME': 'sandeep',  
-#        'USER': '',
-#        'PASSWORD': '',
-#        'HOST': "",  
-#        'PORT': "", 
-#    }
-}
+#DATABASES = {
+##    'default': {
+##        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+##        'NAME': 'sandeep',  
+##        'USER': '',
+##        'PASSWORD': '',
+##        'HOST': "",  
+##        'PORT': "", 
+##    }
+#}
+#DATABASES['default'] =  dj_database_url.config()
+
+import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
+
 
 
 
