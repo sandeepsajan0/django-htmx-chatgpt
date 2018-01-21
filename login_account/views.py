f134452c9075b3cd641d7a67b40d5b28e20fd88e
sandeepsajan0 <sandeepsajan0@gmail.com>
2018-01-22 01:02:10 +0530

    update activation process

--- login_account/views.py
+++ login_account/views.py
@@ -65,7 +65,7 @@ def activate(request, uidb64, token):
     if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
         user.save()
-        login(request, user)
+        #login(request, user)
         # return redirect('home')
         return redirect('/login/home1')
     else:
