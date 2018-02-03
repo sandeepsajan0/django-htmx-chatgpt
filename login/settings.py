3b1fffcc76f4993b15e215075c96cb1d9821823e
sandeepsajan0 <sandeepsajan0@gmail.com>
2018-02-03 18:08:12 +0530

    add south

--- login/settings.py
+++ login/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
 DATABASES['default'] =  dj_database_url.config()
 
 SOUTH_DATABASE_ADAPTERS = {
-    'default': "south.db. postgresql_psycopg2"
+    'default': "south.db.postgresql_psycopg2"
 }
 
 
