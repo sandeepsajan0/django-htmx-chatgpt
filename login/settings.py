be322b993b07592ec71b29cd2592adf8cb850e6e
sandeepsajan0 <sandeepsajan0@gmail.com>
2018-02-03 17:24:37 +0530

    remove south

--- login/settings.py
+++ login/settings.py
@@ -34,7 +34,6 @@ ALLOWED_HOSTS = [".herokuapp.com"]
 
 INSTALLED_APPS = [
     'login_account',
-    'south',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
