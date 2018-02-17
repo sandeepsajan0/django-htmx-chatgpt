d9dd6086c73bfb259514cc9911a14d6907dc4699
sandeepsajan0 <sandeepsajan0@gmail.com>
2018-02-17 14:58:32 +0530

    sss

--- login/settings.py
+++ login/settings.py
@@ -138,7 +138,7 @@ LOGIN_REDIRECT_URL = '/login/profile'
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'sandeepsajan0@gmail.com'
-EMAIL_HOST_PASSWORD = 'asandeep@123'
+EMAIL_HOST_USER = 'sachauhan149@gmail.com'
+EMAIL_HOST_PASSWORD = 'sand@123'
 EMAIL_PORT = 587
 
